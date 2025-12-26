defaa444e90e938437d70d16ed81244b5b36ffe0
LEON-72 <hr.leon72@gmail.com>
2025-12-26 10:42:05 +0900

    feat:保存機能を追加

--- renderer.js
+++ renderer.js
@@ -2,33 +2,60 @@ const input = document.getElementById('todo-input');
 const button = document.getElementById('add-button');
 const list = document.getElementById('todo-list');
 
+// 起動時に保存されたデータを読み込む
+window.addEventListener('DOMContentLoaded', () => {
+    const savedData = JSON.parse(localStorage.getItem('Todos')) || [];
+    savedData.forEach(todoText => {
+        createTodoElement(todoText)
+    });
+});
+
+// 現在の状態を保存する
+function saveTodos() {
+    const todos = [];
+
+    document.querySelectorAll('#todo-list li span').forEach(span => {
+        todos.push(span.textContent);
+    });
+
+    localStorage.setItem('Todos', JSON.stringify(todos));
+    console.log()
+}
+
+// TODOを追加する
+function createTodoElement(text) {
+    // リスト項目(li)を作る
+    const li = document.createElement('li');
+
+    // テキスト用のスパンを作る(文字とボタンを分けるため)
+    const span = document.createElement('span');
+    span.textContent = text;
+    li.appendChild(span);
+
+    // 削除ボタンを作る
+    const deleteButton = document.createElement('button');
+    deleteButton.textContent = '削除';
+    deleteButton.style.marginLeft = '10px';
+
+    // 削除ボタンが押されたとき
+    deleteButton.addEventListener('click', () => {
+        li.remove();
+        saveTodos();
+    });
+
+    li.appendChild(deleteButton);
+    list.appendChild(li);
+
+    input.value = "";
+    console.log("リストに追加完了！");
+}
+
+// 追加ボタンのイベント
 button.addEventListener('click', () => {
     const text = input.value;
-    console.log("ボタンが押されました。入力内容:", text);
-
-    if (text.trim() !== "") {
-        // 1.リスト項目(li)を作る
-        const li = document.createElement('li');
-
-        // 2.テキスト用のスパンを作る(文字とボタンを分けるため)
-        const span = document.createElement('span');
-        span.textContent = text;
-        li.appendChild(span);
-
-        // 3.削除ボタンを作る
-        const deleteButton = document.createElement('button');
-        deleteButton.textContent = '削除';
-        deleteButton.style.marginLeft = '10px';
-
-        // 4.削除ボタンが押されたとき
-        deleteButton.addEventListener('click', () => {
-            li.remove();
-        });
-
-        li.appendChild(deleteButton);
-        list.appendChild(li);
-
+    if(text.trim() !== ""){
+        createTodoElement(text);
+        saveTodos();
         input.value = "";
-        console.log("リストに追加完了！");
     }
 });
